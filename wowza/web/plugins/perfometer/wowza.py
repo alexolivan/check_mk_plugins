33d64b6871dbbd7e20f8201796ce15ee75e81ffa
alexolivan <alejandro.olivan.alvarez@gmail.com>
2016-09-14 16:32:14 +0200

    Plugin tested working. Perfometer is working.

--- wowza/web/plugins/perfometer/wowza.py
+++ wowza/web/plugins/perfometer/wowza.py
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/usr/bin/python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
 # +---------------------------------------------------------------------+
 # |       __        __                          ____  _____             |
 # |       \ \      / /____      ________ _     / ___|| ____|            |
@@ -9,7 +10,7 @@
 # | Copyright Alejandro Olivan 2016                 alex@alexolivan.com |
 # +---------------------------------------------------------------------+
 # | A Check_mk agent to monitor Wowza Stream Engine servers on Linux    |
-# | This file contains plugin agent definition.                         |
+# | This file contains plugin perfometer definition.                    |
 # +---------------------------------------------------------------------+
 #
 # This program is free software; you can redistribute it and/or
@@ -27,30 +28,28 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
 # MA  02110-1301, USA.
 
-# Environment Variables
-host=127.0.0.1
-port=8086
-user="user"
-password="password"
-PARSER="/usr/lib/check_mk_agent/plugins/wowza_xml_parser.py"
-TMPFILE="/tmp/serverinfo"
+def perfometer_wowza(row, check_command, perf_data):
+    current = int(perf_data[0][1])   
+    maxim = int(perf_data[0][6])
+    peak_warn = int(perf_data[0][3])
+    peak_crit = int(perf_data[0][4])
 
-# Check for parser in place or exit right now
-if [ ! -f $PARSER ] ; then
-	echo "FATAL: XML parsing script ${PARSER} not found, plugin is currently useless, so exiting now!"
-    exit 0
-fi
 
-# Check for old data file and clear it if so
-if [ -f $TMPFILE ] ; then
-    rm $TMPFILE
-fi
+    if row["service_state"] == 0:
+        color = "#0f0"
+    elif row["service_state"] == 1:
+        color = "#ff0"
+    elif row["service_state"] == 2:
+        color = "#f00"
+    else:
+        color = "#fa0"
 
-# Get server stats into new tmp xml file
-wget -q --user=${user} --password=${password} http://${host}:${port}/serverinfo -P /tmp
+    usage = int(current / float(maxim) * 100)
+    usage_to_max = int((maxim - usage) / float(maxim) * 100)
 
-# Check new data file is present and parse it if so
-if [ -f $TMPFILE ] ; then
-    echo "<<<wowza>>>"
-    $PARSER $TMPFILE
-fi
+    return "%d%%" % usage, '<table><tr>' \
+                               + perfometer_td(usage, color) \
+                               + perfometer_td(usage_to_max, '#fff') \
+                               + '</tr></table>'
+
+perfometers['check_mk-wowza'] = perfometer_wowza
