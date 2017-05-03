721c33b217ecc52af20739f299d42a758bb55746
Alejandro Olivan Alvarez <alex@icc212.hq.iccbroadcast.com>
2017-05-03 18:30:14 +0200

    Added a new python agent for Icecast2 2.3.x

---
+++ icecast2/agents/plugins/icecast2.py
@@ -0,0 +1,111 @@
+#!/usr/bin/python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+# +---------------------------------------------------------------------+
+# |       ___                       _   ____    _____                   |
+# |      |_ _|___ ___  ___ __ _ ___| |_|___ \  |___ / __  __            |
+# |       | |/ __/ _ \/ __/ _` / __| __| __) |   |_ \ \ \/ /            |
+# |       | | (_|  __/ (_| (_| \__ \ |_ / __/ _ ___) | >  <             |
+# |      |___\___\___|\___\__,_|___/\__|_____(_)____(_)_/\_\            |
+# |                                                                     |
+# |                                                                     |
+# | Copyright Alejandro Olivan 2017                 alex@alexolivan.com |
+# +---------------------------------------------------------------------+
+# | A Check_mk agent to monitor Icecast 2.3.x servers on Linux          |
+# | This file contains plugin agent definition.                         |
+# +---------------------------------------------------------------------+
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+# MA  02110-1301, USA.
+
+import urllib2
+import base64
+import os
+
+customers = (
+    {'host': '192.168.210.105', 'port': '8130', 'mountpoint':'malagafmmobile', 'adminuser': 'admin', 'adminpass': 'MARCAMALAGApass1'},
+    {'host': '192.168.210.108', 'port': '8130', 'mountpoint':'malagafmmobile', 'adminuser': 'admin', 'adminpass': 'MARCAMALAGApass1'},
+	)
+
+results = []
+filelist = []
+
+
+def downloadStatsfiles():
+    for d in customers:
+        url = "http://%s:%s/admin/stats?mount=/%s" % (d['host'], d['port'], d['mountpoint'])
+        filename = "/tmp/icecast-%s-%s" % (d['host'], d['port'])
+        request = urllib2.Request(url)
+        base64string = base64.encodestring('%s:%s' % (d['adminuser'], d['adminpass'])).replace('\n', '')
+        request.add_header("Authorization", "Basic %s" % base64string)
+        try: 
+            s = urllib2.urlopen(request)
+            contents = s.read()
+            file = open(filename, 'w')
+            file.write(contents)
+            file.close()
+            filelist.append({'port': d['port'], 'filename': filename, 'mountpoint': d['mountpoint']})
+        except urllib2.error.URLError as e:
+            pass
+
+
+def parseStatFile(port, file, mountpoint):
+    from xml.dom import minidom
+    xmldoc = minidom.parse(file)
+    totalsources = xmldoc.getElementsByTagName('sources')[0].firstChild.nodeValue
+    if totalsources > 0:
+    	status = 1
+        for source in xmldoc.getElementsByTagName('source'):
+            if source.hasAttribute('mount') and source.getAttribute('mount') == "/%s" % mountpoint:
+                listenersElement = source.getElementsByTagName('listeners')
+                listeners = listenersElement[0].firstChild.nodeValue 
+                peakListenerElement = source.getElementsByTagName('listener_peak')
+                listenerPeak = peakListenerElement[0].firstChild.nodeValue
+            else:
+                listeners = 0
+                listenerPeak = 0
+    else:
+    	status = 0
+        listeners = 0
+        listenerPeak = 0
+    results.append((port, listeners, status, listenerPeak))    	
+
+
+def parseStatFiles():
+    for file in filelist:
+    	parseStatFile(file['port'], file['filename'], file['mountpoint'])
+
+
+def printResults():
+    print('<<<icecast2>>>')
+    for result in results:
+        resultstr = ""
+        for item in result:
+            resultstr+=str(item) + " "
+        print(resultstr)
+
+
+def deleteUsedFiles():
+	for file in filelist:
+		os.remove(file['filename'])
+
+
+def mainFunction():
+    downloadStatsfiles()
+    parseStatFiles()
+    printResults()
+    deleteUsedFiles()
+
+
+mainFunction()
